9ecd25f9353b50f9329e66b00cb4b07ba3541320
nachopitt <nachopitt@gmail.com>
2023-01-18 00:59:34 -0600

    Bootstrap dotfiles in $HOME

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# https://github.com/nachopitt/dotfiles
+if [ ! -d "$HOME/.cfg" ]; then
+    git clone git@github.com:nachopitt/dotfiles.git $HOME/.cfg
+fi
+/usr/bin/git --git-dir=$HOME/.cfg/.git/ --work-tree=$HOME config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/.cfg/.git/ --work-tree=$HOME checkout .
+
 # https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
