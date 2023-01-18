1459f6a2280b966eec6c24ce7a2fe3fe433465d5
nachopitt <nachopitt@gmail.com>
2023-01-18 00:56:54 -0600

    Remove complete-alias git clone from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,3 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # https://github.com/seebi/dircolors-solarized/
 curl -fLo ~/.dircolors --create-dirs \
     https://raw.githubusercontent.com/huyz/dircolors-solarized/master/dircolors.ansi-dark
-
-# https://github.com/cykerway/complete-alias/
-mkdir -p ~/env
-git clone https://github.com/cykerway/complete-alias.git ~/env/complete-alias
