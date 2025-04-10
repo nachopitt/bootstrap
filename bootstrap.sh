a6ced27582a9fca6e8b27012a04aa0e2a58241fe
nachopitt <nachopitt@gmail.com>
2025-04-10 12:38:05 -0600

    Use local yadm instance instead

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,13 +9,17 @@ case "${unameOut}" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 
-sudo apt-get install git vim curl neovim make gcc yadm --yes --quiet
+sudo apt-get install git vim curl neovim make gcc --yes --quiet
 
 # https://github.com/nachopitt/dotfiles
-if [ ! -d $(yadm rev-parse --git-dir) ]; then
+if [ ! -f $HOME/bin/yadm ]; then
+    curl -fLo $HOME/bin/yadm https://github.com/yadm-dev/yadm/raw/master/yadm && chmod a+x $HOME/bin/yadm
+fi
+
+if [ ! -d $($HOME/bin/yadm rev-parse --git-dir) ]; then
     curl -o ssh_config https://raw.githubusercontent.com/nachopitt/dotfiles/main/.ssh/config
 
-    GIT_SSH_COMMAND="ssh -F ssh_config" yadm clone git@github.com:nachopitt/dotfiles.git
+    GIT_SSH_COMMAND="ssh -F ssh_config" $HOME/bin/yadm clone git@github.com:nachopitt/dotfiles.git
 
     rm ssh_config
 fi
