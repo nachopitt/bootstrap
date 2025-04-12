b158d7168f362e3d0dd311908d16c1c5630373e5
nachopitt <nachopitt@gmail.com>
2025-04-12 17:01:41 -0600

    Fix yadm installation bin path

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,14 +12,14 @@ esac
 sudo apt-get install git vim curl neovim make gcc --yes --quiet
 
 # https://github.com/nachopitt/dotfiles
-if [ ! -f $HOME/bin/yadm ]; then
-    curl -fLo $HOME/bin/yadm https://github.com/yadm-dev/yadm/raw/master/yadm && chmod a+x $HOME/bin/yadm
+if [ ! -f $HOME/.local/bin/yadm ]; then
+    curl -fLo $HOME/.local/bin/yadm https://github.com/yadm-dev/yadm/raw/master/yadm && chmod a+x $HOME/.local/bin/yadm
 fi
 
-if [ ! -d $($HOME/bin/yadm rev-parse --git-dir) ]; then
+if [ ! -d $($HOME/.local/bin/yadm rev-parse --git-dir) ]; then
     curl -o ssh_config https://raw.githubusercontent.com/nachopitt/dotfiles/main/.ssh/config
 
-    GIT_SSH_COMMAND="ssh -F ssh_config" $HOME/bin/yadm clone git@github.com:nachopitt/dotfiles.git
+    GIT_SSH_COMMAND="ssh -F ssh_config" $HOME/.local/bin/yadm clone git@github.com:nachopitt/dotfiles.git
 
     rm ssh_config
 fi
